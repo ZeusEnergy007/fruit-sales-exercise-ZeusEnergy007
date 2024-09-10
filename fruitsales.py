a8e9c1d31a2840c52aae2a993079c0fee7dedf3a
STEVEN A BENNETT <stevenbennett@yahoo.com>
2024-09-10 19:22:43 -0400

    Rmv index

--- fruitsales.py
+++ fruitsales.py
@@ -5,4 +5,4 @@ fruit_sales = pd.DataFrame([[35, 21], [41, 34]], columns=['Apples', 'Bananas'],
                 index=['2017 Sales', '2018 Sales'])
 # fruit_sales
 
-fruit_sales.to_csv("fruit.csv")
+fruit_sales.to_csv("fruit.csv",index=False)
