ac7176c91ce8219a03e9a92f8132d98c9d72c155
STEVEN A BENNETT <stevenbennett@yahoo.com>
2024-09-10 13:18:21 -0400

    Fruit.CSV

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,8 @@
 # add your code here
+import pandas as pd
+
+fruit_sales = pd.DataFrame([[35, 21], [41, 34]], columns=['Apples', 'Bananas'],
+                index=['2017 Sales', '2018 Sales'])
+# fruit_sales
+
+fruit_sales.to_csv("fruit.csv")
